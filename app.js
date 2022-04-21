cded795fcae45d5098b56d1dcab7031699772c6f
byaskew3 <byaskew3@gmail.com>
2022-04-21 14:23:52 -0600

    JS

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-fetch('https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=6')
+const btnEl = document.querySelector('.btn')
+
+
+const getColorScheme = () => {
+    let hexCodesHtml = '';
+    let colorsHtml = '';
+    fetch('https://www.thecolorapi.com/scheme?hex=24B1E0&mode=monochrome&count=6')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const colorsArr = data.colors
+
+        for (let color of colorsArr) {
+            colorsHtml += `
+            <li class="color" style="background:${color.hex.value}"></li>
+            `
+        }
+
+        for (let hexCode of colorsArr) {
+            hexCodesHtml += `
+            <li class="hex">${hexCode.hex.value}</li>
+            `
+        }
+
+        document.querySelector('.hex-codes').innerHTML = hexCodesHtml;
+        document.querySelector('.color-palette').innerHTML = colorsHtml;
+        console.log(colorsHtml)
+    })
+
+}
+
+btnEl.addEventListener('click', getColorScheme)
